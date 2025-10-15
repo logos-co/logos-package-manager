a1ad26e98fc9725c37030d5c174d46c63a62dc65
Iuri Matias <iuri.matias@gmail.com>
2025-10-15 13:21:04 -0400

    add example app to verify compiled plugin looks correct

---
+++ example/Readme.md
@@ -0,0 +1,2 @@
+
+nix run --extra-experimental-features nix-command --extra-experimental-features flakes '.#' -- --path <full_path>/logos-package-manager/result/lib/logos/modules/package_manager_plugin.dylib
--- example/plugin_loader.cpp
+++ example/plugin_loader.cpp
@@ -5,6 +5,10 @@
 #include <QObject>
 #include <QtPlugin>
 #include <QString>
+#include <QMetaObject>
+#include <QMetaMethod>
+#include <QMetaProperty>
+#include <QMetaEnum>
 
 // Minimal local declaration of the PluginInterface with the same IID
 class PluginInterface
@@ -61,6 +65,72 @@ int main(int argc, char *argv[])
         const QMetaObject *meta = plugin->metaObject();
         if (meta) {
             qInfo() << "Qt class name:" << meta->className();
+        }
+    }
+
+    // List all available methods in the plugin
+    const QMetaObject *meta = plugin->metaObject();
+    if (meta) {
+        qInfo() << "\n=== Available Methods ===";
+        
+        // List all methods (including inherited ones)
+        for (int i = 0; i < meta->methodCount(); ++i) {
+            QMetaMethod method = meta->method(i);
+            QString methodType;
+            
+            switch (method.methodType()) {
+                case QMetaMethod::Method:
+                    methodType = "Method";
+                    break;
+                case QMetaMethod::Signal:
+                    methodType = "Signal";
+                    break;
+                case QMetaMethod::Slot:
+                    methodType = "Slot";
+                    break;
+                case QMetaMethod::Constructor:
+                    methodType = "Constructor";
+                    break;
+                default:
+                    methodType = "Unknown";
+                    break;
+            }
+            
+            QString accessLevel;
+            if (method.access() == QMetaMethod::Public) {
+                accessLevel = "Public";
+            } else if (method.access() == QMetaMethod::Protected) {
+                accessLevel = "Protected";
+            } else if (method.access() == QMetaMethod::Private) {
+                accessLevel = "Private";
+            } else {
+                accessLevel = "Unknown";
+            }
+            
+            qInfo() << QString("  %1 %2 %3(%4)")
+                       .arg(accessLevel)
+                       .arg(methodType)
+                       .arg(method.name().constData())
+                       .arg(method.parameterNames().join(", "));
+        }
+        
+        qInfo() << "\n=== Properties ===";
+        for (int i = 0; i < meta->propertyCount(); ++i) {
+            QMetaProperty prop = meta->property(i);
+            qInfo() << QString("  Property: %1 (%2)")
+                       .arg(prop.name())
+                       .arg(prop.typeName());
+        }
+        
+        qInfo() << "\n=== Enums ===";
+        for (int i = 0; i < meta->enumeratorCount(); ++i) {
+            QMetaEnum enumerator = meta->enumerator(i);
+            qInfo() << QString("  Enum: %1").arg(enumerator.name());
+            for (int j = 0; j < enumerator.keyCount(); ++j) {
+                qInfo() << QString("    %1 = %2")
+                           .arg(enumerator.key(j))
+                           .arg(enumerator.value(j));
+            }
         }
     }
 
